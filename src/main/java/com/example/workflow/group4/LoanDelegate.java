b943fdad105eb196d0a8c175ba978473eaa9f677
unknown <tomasz.feliks.mlynarczyk@gmail.com>
2022-07-08 12:49:29 +0200

    Tworzenie procesu grupa 4

---
+++ src/main/java/com/example/workflow/group4/LoanDelegate.java
@@ -0,0 +1,46 @@
+package com.example.workflow.group4;
+
+import com.example.workflow.mvc.entity.Client;
+import com.example.workflow.mvc.entity.Debt;
+import com.example.workflow.mvc.service.ClientService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+@Component
+public class LoanDelegate implements JavaDelegate {
+    public static final String CLIENT_ID = "clientId";
+
+    @Inject
+    ClientService clientService;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        Boolean error = true;
+        Long amount = 0L;
+        int retraice = (int) delegateExecution.getVariable("retraice");
+
+        Long clientId = (Long) delegateExecution.getVariable(CLIENT_ID);
+        Optional<Client> clientOpt = clientService.getClientById(clientId);
+        if (clientOpt.isPresent()) {
+            if(retraice==1){
+                throw new RuntimeException("Wykonano zbyt wiele prob!");
+            }
+            retraice--;
+            Client client = clientOpt.get();
+
+            Debt clientDebt = client.getDebt();
+            if (clientDebt != null) {
+                error = false;
+                amount = Long.valueOf(clientDebt.getAmount());
+
+            }
+        }
+        delegateExecution.setVariable("error", error);
+        delegateExecution.setVariable("dept", amount);
+        delegateExecution.setVariable("retraice", retraice);
+    }
+}
--- src/main/java/com/example/workflow/mvc/service/ClientService.java
+++ src/main/java/com/example/workflow/mvc/service/ClientService.java
@@ -2,11 +2,13 @@ package com.example.workflow.mvc.service;
 
 import com.example.workflow.mvc.entity.Client;
 import com.example.workflow.mvc.repository.ClientRepository;
+import com.example.workflow.mvc.repository.DebtRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -16,7 +18,16 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Autowired
+    private DebtRepository debtRepository;
+
     public List<Client> getClients() {
         return clientRepository.findAll();
     }
+
+    public Optional<Client> getClientById(Long clientId) {
+        return clientRepository.findById(clientId);
+    }
+
+
 }
