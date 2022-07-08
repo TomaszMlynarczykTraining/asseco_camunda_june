9629fc87399fcda8e05a98cb92cdbce95c91286d
NobleProg <t>
2022-07-08 08:30:45 +0200

    another example

---
+++ src/main/java/com/example/workflow/mvc/delegates/MockDelegateCor.java
@@ -0,0 +1,37 @@
+package com.example.workflow.mvc.delegates;
+
+
+import com.example.workflow.mvc.service.ClientService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MockDelegateCor implements JavaDelegate {
+
+    @Autowired
+    RuntimeService runtimeService;
+
+    @Autowired
+    ClientService clientService;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+        // System.out.println(delegateExecution.getVariable("pizza"));
+        // delegateExecution.getSuperExecution().getVariable("pizza");
+        // delegateExecution.getVariableLocal("pizza");
+
+//        List<Client> clients = clientService.getClients().stream()
+//                .collect(Collectors.toList());
+//
+//        delegateExecution.setVariable("clientList", clients);
+
+        //System.out.println(runtimeService.getVariable(delegateExecution.getProcessInstanceId(), "pizza"));
+
+        runtimeService.correlateMessage("Message_2i2ib8h");
+
+    }
+}
