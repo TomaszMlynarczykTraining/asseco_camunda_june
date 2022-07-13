ae9895ff89ba7ad3bf001bd8bfcf11c38f111172
NobleProg <piotr.tajdus@asseco.pl>
2022-07-13 09:49:16 +0200

    grupa 3

---
+++ src/main/java/com/example/workflow/group3/CheckCurrencyExternalTaskGRP3.java
@@ -0,0 +1,48 @@
+package com.example.workflow.group3;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.Variables;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.Duration;
+
+@Component
+@ExternalTaskSubscription("checkCurrency")
+public class CheckCurrencyExternalTaskGRP3 implements ExternalTaskHandler {
+
+    @Override
+    public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+        String businessKey = externalTask.getBusinessKey();
+
+        try {
+            //logika biznesowa, np. prztwarzanie pdfa
+            RestTemplateBuilder builder = new RestTemplateBuilder();
+            RestTemplate template = builder.build();
+//        String uri = UriComponentsBuilder.fromUriString("https://api.nbp.pl/api/exchangerates/rates/A/USD/?format=json")
+//                .pathSegment("")
+//                .encode()
+//                .toUriString();
+
+            String url = "https://api.nbp.pl/api/exchangerates/rates/A/USD/?format=json";
+
+            String rate = template.getForObject(url, String.class);
+
+            VariableMap variables = Variables.createVariables().putValue("rate", rate);
+
+            externalTaskService.complete(externalTask, variables);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+            Long duration = Duration.parse("PT2M").toMillis();
+            externalTaskService.handleFailure(externalTask.getId(), "Exception!", ExceptionUtils.getStackTrace(ex), 1, duration);
+        }
+
+    }
+}
